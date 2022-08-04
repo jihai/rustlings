dede6bf5d4a6d583d5d37bbd14691fa1e6ab7fae
jihai <jihaixu@gmail.com>
2022-08-04 06:42:03 +0000

    Until box&arc

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,12 +10,17 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut vec = self.clone();
+        vec.push(String::from("Bar"));
+        vec
+    }
+}
 //TODO: Add your code here
 
 #[cfg(test)]
